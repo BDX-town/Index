000567f8948355d80526a250732fe16a17ccf67e
unknown <clovis@bdx.town>
2023-04-08 17:32:17 +0200

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "index.html"
+  ],
+  theme: {
+    fontFamily: {
+      sans: 'sans-serif',
+    },
+    colors: {
+      accent: "#2ca078"
+    },
+    extend: {
+
+    },
+  },
+  plugins: [],
+}
+
