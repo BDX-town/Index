ad4b504588c6582a806b0365c186ee7575838bbd
admin <admin@Sonar.lan>
2024-06-15 14:13:07 +0200

    feat: load and clean canaille code

--- build.ts
+++ build.ts
@@ -1,11 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/server';
+import { PurgeCSS } from 'purgecss';
+import createCleanCss from 'clean-css';
 
 import { Wrapper } from './src/Wrapper';
+import { StyleMeta } from '@bdxtown/canaille';
 
-const markup = `
-    <!DOCTYPE html>
-    ${ReactDOM.renderToStaticMarkup(React.createElement(Wrapper))}
-`;
+const cleancss = new createCleanCss();
 
-console.log(markup);
+
+
+async function generate() {
+    const markup = `
+<!DOCTYPE html>
+${ReactDOM.renderToStaticMarkup(React.createElement(Wrapper))}
+    `;
+
+    const css = StyleMeta.getServerSideStyle();
+
+    const output = await new PurgeCSS().purge({
+        content: [
+          {
+            raw: markup,
+            extension: 'html'
+          },
+        ],
+        css: [
+          {
+            raw: css
+          }
+        ]
+    })
+    console.log(markup.replace("CANAILLE_STYLE_HERE", cleancss.minify(output[0].css).styles).replace(/@layer components/g, '@layer cmps'))
+}
+
+generate();
